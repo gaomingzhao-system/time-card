9cfcd6b2f3b3a4ea7e95b5e460d5943caa26511f
gaomingzhao-system <m-kou@mldfp.com>
2025-09-13 16:39:36 +0900

    feat: add dialog and alert dialog component

---
+++ src/lib/components/AlertDialog.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import * as AlertDialog from '$lib/components/ui/alert-dialog/index.js'
+    import { buttonVariants } from '$lib/components/ui/button/index.js'
+
+    interface Props {
+        triggerText: string
+        title: string
+        description: string
+    }
+    const props: Props = $props()
+</script>
+
+<AlertDialog.Root>
+    <AlertDialog.Trigger class={buttonVariants({ variant: 'outline' })}>
+        {props.triggerText}
+    </AlertDialog.Trigger>
+    <AlertDialog.Content>
+        <AlertDialog.Header>
+            <AlertDialog.Title>{props.title}</AlertDialog.Title>
+            <AlertDialog.Description>
+                {props.description}
+            </AlertDialog.Description>
+        </AlertDialog.Header>
+        <AlertDialog.Footer>
+            <AlertDialog.Cancel>キャンセル</AlertDialog.Cancel>
+            <AlertDialog.Action>オッケ</AlertDialog.Action>
+        </AlertDialog.Footer>
+    </AlertDialog.Content>
+</AlertDialog.Root>
