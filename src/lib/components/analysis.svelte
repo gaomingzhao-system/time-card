45c170e03f62641ee630c3801a1ef05dfd8852b3
gaomingzhao-system <m-kou@mldfp.com>
2025-09-13 12:46:19 +0900

    dialog and drawer component added

---
+++ src/lib/components/analysis.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import {
+        Card,
+        CardContent,
+        CardHeader,
+        CardTitle,
+    } from '$lib/components/ui/card/index.js'
+    import { Button } from '$lib/components/ui/button/index.js'
+    import * as Drawer from '$lib/components/ui/drawer/index.js'
+
+    const { openText } = $props('')
+</script>
+
+<Drawer.Root>
+    <Drawer.Trigger>{openText}</Drawer.Trigger>
+    <Drawer.Content>
+        <Drawer.Header>
+            <Drawer.Title>Are you sure absolutely sure?</Drawer.Title>
+            <Drawer.Description
+                >This action cannot be undone.</Drawer.Description
+            >
+        </Drawer.Header>
+        <Drawer.Footer>
+            <Button>Submit</Button>
+            <Drawer.Close>Cancel</Drawer.Close>
+        </Drawer.Footer>
+    </Drawer.Content>
+</Drawer.Root>
