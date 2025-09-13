9247b351b5ba2adfec667934ac6147aa07756061
gaomingzhao-system <m-kou@mldfp.com>
2025-09-13 09:25:50 +0900

    first commit

---
+++ prettier.config.js
@@ -0,0 +1,18 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+export default {
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+    plugins: ['prettier-plugin-svelte', 'prettier-plugin-tailwindcss'],
+    tailwindStylesheets: ['./src/app.css'],
+
+    overrides: [
+        {
+            files: '*.svelte',
+            options: { parser: 'svelte' },
+        }
+    ]
+}
