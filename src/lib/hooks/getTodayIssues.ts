0772e45b7c2979050056d76a7122f912cc6e26c8
Nano <gaomingzhao666@outlook.com>
2025-09-15 16:30:34 +0900

    feat: get issues from github

--- src/lib/hooks/getTodayIssues.ts
+++ src/lib/hooks/getTodayIssues.ts
@@ -1,18 +1,18 @@
-// Octokit.js
-
 import { Octokit } from 'octokit'
 
-// https://github.com/octokit/core.js#readme
-const octokit = new Octokit({
-    auth: localStorage.getItem('githubToken'),
-})
-
 const timeStamp = new Date().toISOString()
-export const issues = await octokit.request(
-    `GET /issues?state=closed&since=${timeStamp}`,
-    {
-        headers: {
-            'X-GitHub-Api-Version': '2022-11-28',
+export const getTodayFinishedIssues = async (githubToken: string) => {
+    // https://github.com/octokit/core.js#readme
+    const octokit = new Octokit({
+        auth: localStorage.getItem('githubToken'),
+    })
+    const issues = await octokit.request(
+        `GET /issues?state=closed&since=${timeStamp}`,
+        {
+            headers: {
+                'X-GitHub-Api-Version': '2022-11-28',
+            },
         },
-    },
-)
+    )
+    return issues.data
+}
