a48fca19ff77172f8e230d0139f3efd5cf395b24
gaomingzhao-system <m-kou@mldfp.com>
2025-09-16 13:08:20 +0900

    chore: readability and maintainablity optimizations

--- src/lib/hooks/generateDailyReport.ts
+++ src/lib/hooks/generateDailyReport.ts
@@ -4,6 +4,9 @@ import { GoogleGenAI } from '@google/genai'
 // The client gets the API key from the environment variable `GEMINI_API_KEY`.
 const ai = new GoogleGenAI({ apiKey: import.meta.env.VITE_GEMINI_API_KEY })
 
+const unifiedPrompt =
+    '```commandこのアカウントの今日のissueやpull requestの歴史を使って、今日しましたタスクをリストに書いてください、リンクとissue番号は不要です、特にissueに対応されたタスクのタイトルを書いてください。そして今日の感想も下に書いてください、フォーマットは下の内容にようにしてください：```\n'
+
 // refers to https://www.reddit.com/r/ChatGPTPro/comments/1fgo5ge/is_there_a_benefit_to_writing_to_chatgpt_with/
 // At the very least you need a way to clearly highlight which parts of your prompt are instructions and which parts of your prompt are data (eg reference docs, sample rows, labeled examples). And markdown is expressive enough to give you lots of nice options for that level of distinction (code blocks, headers, etc)
 export const generateDailyReport = async (
@@ -16,6 +19,7 @@ export const generateDailyReport = async (
     const dailyReport = await ai.models.generateContent({
         model: model ?? 'gemini-2.5-flash',
         contents:
+            unifiedPrompt +
             promptsTemplate +
             '\n```issue' +
             todayFinishedIssues.title +
