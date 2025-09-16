9d481783f54d1f5798665a1e8aaafe00b3d2d7d1
gaomingzhao-system <m-kou@mldfp.com>
2025-09-16 15:56:12 +0900

    add some comment

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,9 @@
 // See: https://svelte.dev/docs/kit/single-page-apps
 // See: https://v2.tauri.app/start/frontend/sveltekit/ for more info
 import adapter from '@sveltejs/adapter-vercel'
+// if you wanna build it as a macos app, apply the code below, on reverse, you can use adapter-vercel to deploy it as a web app
+// macos appでしたら、下記のコードを適用してください。逆に、webアプリとしてデプロイしたい場合は、adapter-vercelを使用してください。
+// import  adapter  from "@sveltejs/adapter-static";
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 /** @type {import('@sveltejs/kit').Config} */
