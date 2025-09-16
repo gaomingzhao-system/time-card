43b6dd419c3b58631d93d50f7f7ecf9a0a9cc0e9
gaomingzhao-system <m-kou@mldfp.com>
2025-09-16 13:03:25 +0900

    fix: markdown syntax for better ai comprehension

--- src/lib/hooks/generateDailyReport.ts
+++ src/lib/hooks/generateDailyReport.ts
@@ -2,7 +2,7 @@ import { getTodayFinishedIssues } from './getTodayIssues'
 import { GoogleGenAI } from '@google/genai'
 
 // The client gets the API key from the environment variable `GEMINI_API_KEY`.
-const ai = new GoogleGenAI({ apiKey: 'asd' })
+const ai = new GoogleGenAI({ apiKey: import.meta.env.VITE_GEMINI_API_KEY })
 
 // refers to https://www.reddit.com/r/ChatGPTPro/comments/1fgo5ge/is_there_a_benefit_to_writing_to_chatgpt_with/
 // At the very least you need a way to clearly highlight which parts of your prompt are instructions and which parts of your prompt are data (eg reference docs, sample rows, labeled examples). And markdown is expressive enough to give you lots of nice options for that level of distinction (code blocks, headers, etc)
@@ -11,11 +11,18 @@ export const generateDailyReport = async (
     model: 'gemini-2.5-flash' | 'gemini-2.5-pro',
     promptsTemplate: string,
 ) => {
-    const todayFinishedIssues = getTodayFinishedIssues(githubToken)
+    const todayFinishedIssues = await getTodayFinishedIssues(githubToken)
 
     const dailyReport = await ai.models.generateContent({
-        model: model,
-        contents: promptsTemplate + todayFinishedIssues,
+        model: model ?? 'gemini-2.5-flash',
+        contents:
+            promptsTemplate +
+            '\n```issue' +
+            todayFinishedIssues.title +
+            '```' +
+            '\n```issue body' +
+            todayFinishedIssues.body +
+            '```',
     })
     return dailyReport.text
 }
